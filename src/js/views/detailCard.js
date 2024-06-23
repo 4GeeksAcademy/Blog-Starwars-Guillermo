447c76286fd5005665c71caca71c3804556c2242
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-06-23 21:44:25 +0100

    limpiamos y optimizamos codigo detailCard

--- src/js/views/detailCard.js
+++ src/js/views/detailCard.js
@@ -13,8 +13,6 @@ const DetailCard = () => {
     store.Vehiculos.find((element) => element._id === params.id) ||
     store.Planetas.find((element) => element._id === params.id);
 
-  console.log(element);
-
   const imagenSelect = (description) => {
     if (description === "A person within the Star Wars universe") {
       return "characters";
@@ -22,6 +20,52 @@ const DetailCard = () => {
       return "vehicles";
     } else {
       return "planets";
+    }
+  };
+
+  const getImageSource = () => {
+    if (element._id === "5f7254c11b7dfa00041c6fae") {
+      return store.ImagenesPlanetas.tatooine;
+    } else {
+      const type = imagenSelect(element.description);
+      return `https://starwars-visualguide.com/assets/img/${type}/${element.uid}.jpg`;
+    }
+  };
+
+  const renderProperties = () => {
+    switch (element.description) {
+      case "A person within the Star Wars universe":
+        return (
+          <>
+            <li>Gender: {element.properties.gender}</li>
+            <li>Height: {element.properties.height}</li>
+            <li>Eye color: {element.properties.eye_color}</li>
+            <li>Birth year: {element.properties.birth_year}</li>
+            <li>Mass: {element.properties.mass}</li>
+          </>
+        );
+      case "A vehicle":
+        return (
+          <>
+            <li>Model: {element.properties.model}</li>
+            <li>Cargo capacity: {element.properties.cargo_capacity}</li>
+            <li>Cost in credits: {element.properties.cost_in_credits}</li>
+            <li>Manufacturer: {element.properties.manufacturer}</li>
+            <li>Vehicle class: {element.properties.vehicle_class}</li>
+          </>
+        );
+      case "A planet.":
+        return (
+          <>
+            <li>Climate: {element.properties.climate}</li>
+            <li>Diameter: {element.properties.diameter}</li>
+            <li>Gravity: {element.properties.gravity}</li>
+            <li>Population: {element.properties.population}</li>
+            <li>Terrain: {element.properties.terrain}</li>
+          </>
+        );
+      default:
+        return null;
     }
   };
 
@@ -53,71 +97,43 @@ const DetailCard = () => {
         height: "100vh",
       }}
     >
-      <div className=" d-flex justify-content-center gap-3 mt-5">
+      <div className="d-flex justify-content-center gap-3 mt-5">
         <div>
           <img
-            src={
-              element._id == "5f7254c11b7dfa00041c6fae"
-                ? store.ImagenesPlanetas.tatooine
-                : `https://starwars-visualguide.com/assets/img/${imagenSelect(
-                    element.description
-                  )}/${element.uid}.jpg`
-            }
+            src={getImageSource()}
             className="card-img-top"
             alt={element.properties.name}
           />
         </div>
         <div>
           <h1>{element.properties.name}</h1>
-          <ul>
-            {element.description ===
-              "A person within the Star Wars universe" && (
-              <>
-                <li>Gender: {element.properties.gender}</li>
-                <li>Height: {element.properties.height}</li>
-                <li>Eye color: {element.properties.eye_color}</li>
-                <li>Birth year: {element.properties.birth_year}</li>
-                <li>Mass: {element.properties.mass}</li>
-              </>
-            )}
-            {element.description === "A vehicle" && (
-              <>
-                <li>Model: {element.properties.model}</li>
-                <li>Cargo capacity: {element.properties.cargo_capacity}</li>
-                <li>Cost in credits: {element.properties.cost_in_credits}</li>
-                <li>Manufacturer: {element.properties.manufacturer}</li>
-                <li>Vehicle class: {element.properties.vehicle_class}</li>
-              </>
-            )}
-            {element.description === "A planet." && (
-              <>
-                <li>Climate: {element.properties.climate}</li>
-                <li>Diameter: {element.properties.diameter}</li>
-                <li>Gravity: {element.properties.gravity}</li>
-                <li>Population: {element.properties.population}</li>
-                <li>Terrain: {element.properties.terrain}</li>
-              </>
-            )}
-          </ul>
+          <ul>{renderProperties()}</ul>
           <div>
-            {store.Favoritos.find((fav) => fav._id === element._id) ? (
-              <button
-                className="btn"
-                onClick={() => actions.deleteFav(element._id)}
-              >
-                <i
-                  className="fa-solid fa-bookmark fa-xl"
-                  style={{ color: "#fafafa" }}
-                ></i>
-              </button>
-            ) : (
-              <button className="btn" onClick={() => actions.addFav(element)}>
-                <i
-                  className="fa-regular fa-bookmark fa-xl"
-                  style={{ color: "#ffffff" }}
-                ></i>
-              </button>
-            )}
+            <button
+              className="btn"
+              onClick={() =>
+                actions[
+                  store.Favoritos.find((fav) => fav._id === element._id)
+                    ? "deleteFav"
+                    : "addFav"
+                ](element)
+              }
+            >
+              <i
+                className={`fa-${
+                  store.Favoritos.find((fav) => fav._id === element._id)
+                    ? "solid"
+                    : "regular"
+                } fa-bookmark fa-xl`}
+                style={{
+                  color: `${
+                    store.Favoritos.find((fav) => fav._id === element._id)
+                      ? "#fafafa"
+                      : "#ffffff"
+                  }`,
+                }}
+              ></i>
+            </button>
           </div>
         </div>
       </div>
