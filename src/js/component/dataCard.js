054c88d0c3f73deda490d278cce70d5eb796ce71
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-06-23 21:28:43 +0100

    limpiamos y optimizamos dataCard

--- src/js/component/dataCard.js
+++ src/js/component/dataCard.js
@@ -4,8 +4,21 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 const DataCard = ({ dataList }) => {
-  if (!dataList) return null;
   const { store, actions } = useContext(Context);
+
+  const getImageSource = (data) => {
+    if (data._id === "5f7254c11b7dfa00041c6fae") {
+      return store.ImagenesPlanetas.tatooine;
+    } else {
+      const type =
+        data.description === "A person within the Star Wars universe"
+          ? "characters"
+          : "vehicles";
+      return `https://starwars-visualguide.com/assets/img/${imagenSelect(
+        data.description
+      )}/${data.uid}.jpg`;
+    }
+  };
 
   const imagenSelect = (element) => {
     if (element === "A person within the Star Wars universe") {
@@ -16,8 +29,21 @@ const DataCard = ({ dataList }) => {
       return "planets";
     }
   };
-  //<i clasName="fa-solid fa-bookmark fa-xl" style={{ color: "#fafafa" }}></i>
-  //<i className="fa-regular fa-bookmark fa-xl" style={{ color: "#ffffff" }}></i>
+
+  const isFavorite = (id) => {
+    return store.Favoritos.some((element) => element._id === id);
+  };
+
+  const handleFavoriteClick = (data) => {
+    if (isFavorite(data._id)) {
+      actions.deleteFav(data._id);
+    } else {
+      actions.addFav(data);
+    }
+  };
+
+  if (!dataList || dataList.length === 0) return null;
+
   return (
     <>
       {dataList.map((data) => (
@@ -27,53 +53,40 @@ const DataCard = ({ dataList }) => {
         >
           <div className="card h-100 w-75 bg-transparent">
             <div
-              className="d-flex justify-content-center bg-transparent rounded"
-              style={{
-                overflow: "hidden",
-                height: "270px",
-              }}
+              className="d-flex justify-content-center rounded bg-transparent"
+              style={{ overflow: "hidden", height: "270px" }}
             >
               <img
-                src={
-                  data._id == "5f7254c11b7dfa00041c6fae"
-                    ? store.ImagenesPlanetas.tatooine
-                    : `https://starwars-visualguide.com/assets/img/${imagenSelect(
-                        dataList[0].description
-                      )}/${data.uid}.jpg`
-                }
+                src={getImageSource(data)}
                 className="img-fluid"
                 alt={data.properties.name}
               />
             </div>
-            <div className="bg-dark rounded text-light card-body">
-              <div className=" ">
+            <div className="card-body bg-dark text-light rounded">
+              <div>
                 <h5 className="card-title">{data.properties.name}</h5>
                 <p className="card-text">{data.description}</p>
               </div>
-              <div className="p-2 d-flex justify-content-between bg-dark">
-                <Link to={`/detailCard/${data._id}`}>
-                  <button type="button" className="btn btn-info">
-                    Info
-                  </button>
+              <div className="p-2 d-flex justify-content-between">
+                <Link to={`/detailCard/${data._id}`} className="btn btn-info">
+                  Info
                 </Link>
-                {store.Favoritos.find((element) => element._id === data._id) ? (
-                  <button
-                    className="btn"
-                    onClick={() => actions.deleteFav(data._id)}
-                  >
+                <button
+                  className="btn"
+                  onClick={() => handleFavoriteClick(data)}
+                >
+                  {isFavorite(data._id) ? (
                     <i
                       className="fa-solid fa-bookmark fa-xl"
                       style={{ color: "#fafafa" }}
                     ></i>
-                  </button>
-                ) : (
-                  <button className="btn" onClick={() => actions.addFav(data)}>
+                  ) : (
                     <i
                       className="fa-regular fa-bookmark fa-xl"
                       style={{ color: "#ffffff" }}
                     ></i>
-                  </button>
-                )}
+                  )}
+                </button>
               </div>
             </div>
           </div>
